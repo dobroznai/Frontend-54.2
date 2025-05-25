e307f9759c221d078022471298db4eac622ff477
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-05-25 23:11:57 +0200

    12 DOM refactor

--- front_end/12_DOM/12_lesson/index.js
+++ front_end/12_DOM/12_lesson/index.js
@@ -35,14 +35,14 @@ function result(userGender, height) {
   if (checkingData(height, userGender)) {
     let idealWeight;
     if (userGender === "Male") {
-      idealWeight = height - (100 + (height - 100) / 10;
-      resultId.textContent = "Идеальный вес (мужчина):",
-        idealWeight.toFixed(3),
+      idealWeight = height - (100 + (height - 100) / 10);
+      (resultId.textContent = "Идеальный вес (мужчина):") +
+        idealWeight.toFixed(3) +
         "кг";
     } else {
       idealWeight = height - (100 + (height - 100) / 20);
-      resultId.textContent = "Идеальный вес (женщина):",
-        idealWeight.toFixed(3),
+      (resultId.textContent = "Идеальный вес (женщина):") +
+        idealWeight.toFixed(3) +
         "кг";
     }
   } else {
