d65dbe2be3e85d9aa6c491e6ad01e2047b11c666
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-05-26 12:37:30 +0200

    12 homework correct

--- front_end/12_DOM/12_lesson/index.js
+++ front_end/12_DOM/12_lesson/index.js
@@ -35,13 +35,13 @@ function result(userGender, height) {
   if (checkingData(height, userGender)) {
     let idealWeight;
     if (userGender === "Male") {
-      idealWeight = height - (100 + (height - 100) / 10);
-      resultId.textContent =
-        "Идеальный вес (мужчина): " + idealWeight.toFixed(3) + " кг";
-    } else {
       idealWeight = height - (100 + (height - 100) / 20);
       resultId.textContent =
-        "Идеальный вес (женщина): " + idealWeight.toFixed(3) + " кг";
+        "Ваш идеальный вес: " + idealWeight.toFixed(3) + " кг";
+    } else {
+      idealWeight = height - (100 + (height - 100) / 10);
+      resultId.textContent =
+        "Ваш идеальный вес: " + idealWeight.toFixed(3) + " кг";
     }
   } else {
     resultId.textContent = "Введите верные данные";
